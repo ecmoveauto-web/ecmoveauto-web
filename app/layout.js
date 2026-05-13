375adf7d939b4345c3a7b3a1f5c2941f3be28010
ecmoveauto-web <ecmoveauto@gmail.com>
2026-05-13 13:29:14 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "EC MOVE AUTO",
+  description: "Convoyage automobile premium",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
